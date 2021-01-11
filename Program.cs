1f8e192e3053e9ce932db8a4728d748642a24624
unknown <pedrofelipbarros@gmail.com>
2021-01-11 16:22:47 -0300

    exercicio1 23.11 finalizado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeCliente;
+            int idade;
+
+            Console.WriteLine("Verifique se você já tem idade para votar, adicione seu ano de nascimento a baixo:");
+            idadeCliente = int.Parse(Console.ReadLine());
+            idade = 2020 - idadeCliente;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine($"Sua idade já corresponde ao voto obrigatorio, compareça a sua zona eleitoral");
+            }
+            else if(idade >=16 && idade<=17){
+                Console.WriteLine("Sua idade já permite que você vote mas o criterio fica a sua escolha");
+            }
+            else
+            {
+                Console.WriteLine("Sua idade ainda não permite que você vote");
+            }
+        }
+    }
+}
